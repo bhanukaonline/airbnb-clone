ef998f3aab42fc3225aa76c967f5fef7b564b7ab
Bhanuka Senevirathne <bhanuka76@gmail.com>
2023-08-09 14:09:02 +0530

    tailwindcss setup

--- airbnb/app/page.tsx
+++ airbnb/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>Hello Achala</div>
+    <div className='text-rose-500 text-2xl'>Hello Achala</div>
     
   )
 }
