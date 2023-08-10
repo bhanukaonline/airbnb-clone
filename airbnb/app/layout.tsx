e1fda9b05a3551633ee88180c210e0eac1c3d92c
Bhanuka Senevirathne <bhanuka76@gmail.com>
2023-08-10 14:30:44 +0530

    modal

--- airbnb/app/layout.tsx
+++ airbnb/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Modal isOpen />
+          <Navbar/>
+        </ClientOnly>
+        
         {children}
         </body>
     </html>
